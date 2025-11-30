e6957d1c87cab546507d493457c9a80eb1331a09
Toko Migineishvili <toko.migine@gmail.com>
2025-11-30 23:11:36 +0400

    Update axios.js

--- portfolio/src/lib/axios.js
+++ portfolio/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://portfolio-5helo4h80-toko122s-projects.vercel.app/api'
+    baseURL: '/api'
 })
 
 export default instance
