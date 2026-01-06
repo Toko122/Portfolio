d1910618ee10b6ed618cee126d4f6d701e54a58e
Toko Migineishvili <toko.migine@gmail.com>
2026-01-06 14:47:02 +0400

    Update ProjectSection.js

--- portfolio/src/components/projects/ProjectSection.js
+++ portfolio/src/components/projects/ProjectSection.js
@@ -8,11 +8,11 @@ const ProjectSection = () => {
 
      const projects = [
         {name: 'Blog', image: '/Screenshot 2025-11-29 133837.png', link: 'https://nextblog-ten-chi.vercel.app/', time: '11.25.2025', description: 'Modern blogging platform where users can create and publish posts with a clean and minimal UI.'},
+        {name: 'ChatApp', image: '/Screenshot 2026-01-05 171059.png', link: 'https://chat-app-six-liard-14.vercel.app/', time: '01.05.2026', description: 'Full-Stack Real-Time Chat App with Socket.IO & TypeScript'},
         {name: 'Advanced-Ecommerace', image: '/Screenshot 2025-12-16 151034.png', link: 'https://advanced-ecommerce-ruby.vercel.app/', time: '11.25.2025', description: 'Modern full-stack e-commerce app with dynamic products, cart system, and responsive UI built for real-world scalability.'},
         {name: 'Ecommerce', image: '/Screenshot 2025-11-29 133720.png', link: 'https://ecommerce-nine-beige-73.vercel.app/', time: '08.11.2025', description: 'Fully functional eCommerce website with product listings, cart management, and an intuitive shopping experience.'},
         {name: 'Ecommerce 2', image: '/Screenshot 2025-11-29 134417.png', link: 'https://next-buy-eight.vercel.app/', time: '10.2.2025', description: 'Advanced eCommerce platform designed with improved UI/UX, category filtering, and smooth product browsing.'},
         {name: 'MedCenter', image: '/Screenshot 2025-11-29 233148.png', link: 'https://med-center-react.vercel.app/', time: '03.27.2025', description: 'A fully frontend-built medical clinic platform featuring a clean and responsive UI. The interface includes service listings, doctor sections, and a smooth appointment booking flow — all implemented purely on the client side without backend logic. Designed with modern styling, intuitive navigation, and an optimized user experience across devices.'},
-        {name: 'ChatApp', image: '/Screenshot 2026-01-05 171059.png', link: 'https://chat-app-six-liard-14.vercel.app/', time: '01.05.2026', description: 'Full-Stack Real-Time Chat App with Socket.IO & TypeScript'},
      ]
 
      const router = useRouter()
